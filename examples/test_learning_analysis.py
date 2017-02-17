edfc6ff6006bdf83f9d894da8ce330243dd862a3
ccnlab <marcel.van.gerven@gmail.com>
2017-02-17 10:09:04 +0100

    update

--- examples/test_learning_analysis.py
+++ examples/test_learning_analysis.py
@@ -8,11 +8,8 @@ from learners.base import Learner, Tester
 from learners.iterators import *
 from learners.supervised_learner import StatelessTrainer
 from models.models import Regressor
-from models.monitor import Monitor
 from models.networks import MLP
-import matplotlib.animation as manimation
 import matplotlib.cm as cm
-import scipy.stats as ss
 from analysis.tools import movie
 
 # parameters
@@ -68,55 +65,5 @@ def foo(monitor):
         plt.hold('on')
     plt.axis('off')
 
+# create movie
 movie(snapshots, data, model, foo, "./result/movie.mp4", dpi=100, fps=1)
-
-
-# fig = plt.figure()
-#
-# # get video writer
-# FFMpegWriter = manimation.writers['ffmpeg']
-# metadata = dict(title='Movie Test', artist='CCNLab')
-# writer = FFMpegWriter(fps=1, metadata=metadata)
-#
-#
-# fig = plt.figure()
-#
-# fig.set_size_inches(10, 10)
-#
-# # save frames
-# with writer.saving(fig, "./result/movie.mp4", 100):
-#
-#     for i in range(len(snapshots)):
-#
-#         print 'processing snapshot {0} of {1}'.format(i+1, len(snapshots))
-#
-#         # load snapshot
-#         model.load(snapshots[i])
-#
-#         # set monitor for snapshot
-#         model.set_monitor(Monitor())
-#
-#         # run some test data
-#         Tester(model, SequentialIterator(RegressionDataset(), batch_size=1)).run()
-#
-#         # extract variables
-#         Y = model.monitor.get('prediction')
-#         T = model.monitor.get('target')
-#         [n_samples, n_vars] = Y.shape
-#
-#         colors = cm.rainbow(np.linspace(0, 1, n_vars))
-#         regs = []
-#         for i in range(n_vars):
-#             l = plt.scatter(T[:, i], Y[:, i], c=colors[i, :])
-#             plt.hold('on')
-#         plt.axis('off')
-#
-#         R = np.zeros([n_vars, 1])
-#         for i in range(n_vars):
-#             R[i] = ss.pearsonr(np.squeeze(T[:, i]), np.squeeze(Y[:, i]))[0]
-#
-#         writer.grab_frame()
-#
-#         plt.clf()
-#
-# plt.close()
